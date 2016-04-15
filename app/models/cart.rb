8f51462622070a3a5508b7aae9b82e5f578c2321
Louiefigz <figueroa.aaron@gmail.com>
2016-04-15 11:54:11 -0400

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,15 +5,34 @@ class Cart < ActiveRecord::Base
   belongs_to :user
 
 
+  # def add_item(item_id)
+  #   item = line_items.find_by(item_id: item_id)
+  #           # lineitem_(item_id: 19, cart_id: )
+  #   if item
+  #     item.quantity += 1
+  #   else
+  #     item = line_items.create(item_id: item_id)
+  #   end
+  #   item.save
+  #   item
+  # end
+
+  # This would be similar to
+# cart.items.pluck(:id)
+
+# item_ids
+#Which is similar to:
+# items.each blah blah
+
+
   def add_item(item_id)
-    item = line_items.find_by(item_id: item_id)
-            # lineitem_(item_id: 19, cart_id: )
-    if item
-      item.quantity += 1
+    if item_ids.include?(item_id.to_i)
+      line_item = line_items.find_by(item_id)
+      line_item.update(quantity: line_item.quantity + 1)
+      line_item
     else
-      item = line_items.create(item_id: item_id)
+      line_items.build(item_id: item_id, quantity: 1)
     end
-    item
   end
 
   def total
